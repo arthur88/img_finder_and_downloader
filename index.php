3f2f4d1dc8beeb3f6c7c964eee38f8fca523d46a
arthur88 <info@techgin.com>
2015-05-26 15:26:22 +0300

    simple img finder & downloader

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+function save_image($url){
+ $exp = explode('/', $url);
+ $ch = curl_init($url);
+ $fp = fopen(getcwd().'/imgs/'. $exp[4].'', 'wb');
+ curl_setopt($ch, CURLOPT_FILE, $fp);
+ curl_setopt($ch, CURLOPT_HEADER, 0); 
+ curl_exec($ch);
+ curl_close($ch);
+ fclose($fp);
+}
+
+for($i = 0; $i <= 9999; $i++){
+  $url = 'http://www.automokyklos.lt/testas/teorijos_testas_'.$i.'.jpg';
+  $img_size = GetImageSize($url);
+  if (!empty($img_size)) {  save_image($url);   } else {}
+}
+
